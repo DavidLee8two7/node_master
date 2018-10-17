3294918144e599027c4587efd76bc44aaabc1beb
David Lee <DavidLee8two7@gmail.com>
2018-10-17 16:02:49 -0700

    pug templating language

--- Node_MasterPC/routes/index.js
+++ Node_MasterPC/routes/index.js
@@ -3,7 +3,20 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const david = { name: 'David', age: 1, cool: true };
+  // res.send('Hey! It works!');
+  // res.json(david);
+  // res.send(req.query.name);
+  // res.json(req.query);
+  res.render('hello', {
+    name: 'David',
+    dog: req.query.dog
+  })
 });
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
+})
 
 module.exports = router;
