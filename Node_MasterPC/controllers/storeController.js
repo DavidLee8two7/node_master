ec8aa2907862f151b6f908c7a13612e65bd98a38
David Lee <DavidLee8two7@gmail.com>
2018-10-17 20:15:44 -0700

    cookie parser

--- Node_MasterPC/controllers/storeController.js
+++ Node_MasterPC/controllers/storeController.js
@@ -1,3 +1,10 @@
+exports.myMiddleware = (req, res, next) => {
+    req.name = 'David';
+    res.cookie('name', 'David is cool', { maxAge: 9000000 });
+    next();
+}
+
 exports.homePage = (req, res) => {
+    console.log(req.name)
     res.render('index');
 }
