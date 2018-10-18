89471f9344ea7e8c2a43365c8db7bcfaf171f652
David Lee <DavidLee8two7@gmail.com>
2018-10-18 08:34:27 -0700

    mongoose Store Model

--- Node_MasterPC/controllers/storeController.js
+++ Node_MasterPC/controllers/storeController.js
@@ -1,12 +1,3 @@
-exports.myMiddleware = (req, res, next) => {
-    req.name = 'David';
-    if(req.name === 'David') {
-        throw Error('That is a stupid name')
-    }
-    next();
-}
-
 exports.homePage = (req, res) => {
-    console.log(req.name)
     res.render('index');
 }
