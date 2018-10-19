5431617c6a125a261c20087894ae6da444141c65
David Lee <DavidLee8two7@gmail.com>
2018-10-19 16:37:17 -0700

    Querying Database for Stores

--- Node_MasterPC/controllers/storeController.js
+++ Node_MasterPC/controllers/storeController.js
@@ -13,4 +13,10 @@ exports.createStore = async (req, res) => {
     const store = await (new Store(req.body)).save();
     req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
     res.redirect(`/store/${store.slug}`);
-} 
+}
+
+exports.getStores = async (req, res) => {
+    // 1. Query the database for a list of all stores
+    const stores = await Store.find();
+    res.render('stores', { title: 'Stores', stores});
+}
