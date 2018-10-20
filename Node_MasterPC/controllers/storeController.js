8763167d9bdd8f9584479d4bf0105c9486291b02
David Lee <DavidLee8two7@gmail.com>
2018-10-20 14:06:54 -0700

    Adding/editing stores with one file

--- Node_MasterPC/controllers/storeController.js
+++ Node_MasterPC/controllers/storeController.js
@@ -6,7 +6,7 @@ exports.homePage = (req, res) => {
 };
 
 exports.addStore = (req, res) => {
-    res.render('editStore', { title: '🧖‍♂️Add Store' });
+    res.render('editStore', { title: 'Add Store' });
 };
 
 exports.createStore = async (req, res) => {
@@ -26,5 +26,19 @@ exports.editStore = async (req, res) => {
     const store = await Store.findOne({_id: req.params.id});
     // 2. confirm they are the owner of the store
     // 3. Render out the edit form so the user can update their store
-    res.render('editStore', { title: `Edit ${store.name}`, store})
+    res.render('editStore', { title: `Edit ${store.name}`, store});
+};
+
+exports.updateStore = async (req, res) => {
+    // set the location data to be a point
+    req.body.location.type = 'Point';
+    // find and update the store
+    const store = await Store.findOneAndUpdate({_id: req.params.id},
+    req.body, {
+        new: true, // return the new store instead of the old one
+        runValidators: true
+    }).exec();
+    req.flash('success', `Successfully updated <strong>${store.name}</stong>. <a href="/stores/${store.slug}">View Store 🚣‍♂️</a>`);
+    res.redirect(`/stores/${store._id}/edit`);
+    // Redirect them to the store and tell them it worked
 };
